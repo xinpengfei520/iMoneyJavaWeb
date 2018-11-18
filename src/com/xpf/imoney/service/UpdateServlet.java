5990ba969155906e528c1a522bc678a9699cff57
xinpengfei <542270559@qq.com>
2018-11-18 16:26:34 +0800

    update json interface.

---
+++ src/com/xpf/imoney/service/UpdateServlet.java
@@ -0,0 +1,38 @@
+package com.xpf.imoney.service;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@WebServlet(name = "UpdateServlet", urlPatterns = "/update")
+public class UpdateServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        OutputStream out = response.getOutputStream();
+        String path = getServletContext().getRealPath("/")
+                + "json/update.json";
+        File file = new File(path);
+        FileInputStream stream = new FileInputStream(file);
+        int count = -1;
+        byte[] buffer = new byte[1024];
+        while ((count = stream.read(buffer)) != -1) {
+            out.write(buffer, 0, count);
+            out.flush();
+        }
+        stream.close();
+        out.close();
+    }
+
+}
